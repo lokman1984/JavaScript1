474a00a111da6c3711763b8ba757cd9f420c4a8a
lokman dlimi <48956227+lokman1984@users.noreply.github.com>
2020-01-24 18:50:10 +0100

    Update step3.js

--- Week1/homework/step3.js
+++ Week1/homework/step3.js
@@ -1,8 +1,7 @@
 "use strict";
 let x;
-console.log("the value of my variable x will be a number");
+console.log(x);
+console.log("the value of my variable x will be a undefined");
 x = 1;
 console.log(x);
-x ++;
-console.log("the value of my variable x will be another number");
-console.log(x);
+console.log("the value of my variable x will be  number");
