46c3ca68cdd51ad1b8b46da3fe18fea67ea75de5
lokman dlimi <48956227+lokman1984@users.noreply.github.com>
2020-01-25 10:40:59 +0100

    Update step4.js
    I appologise to you ,but i thought that what was required was more complicated than this and not simple

--- Week1/homework/step4.js
+++ Week1/homework/step4.js
@@ -1,7 +1,7 @@
 let myString = "Lokman Dlimi";
-console.log("the value of my string will be:" + typeof myString);
+console.log("the value of my string will be: lokman Dlimi");
 console.log( myString);
-myString = 34;
-console.log("the value of my string will be:" + typeof myString);
+myString = "Dlimi";
+console.log("the value of my string will be:Dlimi");
 console.log( myString);
 
